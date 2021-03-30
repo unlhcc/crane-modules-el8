a6b28a86ceb3d9d8b6e70d91f1ba40f644b7c7e6
Adam Caprez <acaprez2@unl.edu>
2021-03-30 20:36:14 +0000

    Fix szip location.

--- Compiler/intel/19/szip/2.1.lua
+++ Compiler/intel/19/szip/2.1.lua
@@ -14,11 +14,11 @@ whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://www.hdfgroup.org/doc_resource/SZIP")
 
-prepend_path("LD_LIBRARY_PATH",     "/util/opt/szip/2.1.1/intel/19/lib")
-prepend_path("INCLUDE",             "/util/opt/szip/2.1.1/intel/19/include")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/szip/2.1/intel/19/lib")
+prepend_path("INCLUDE",             "/util/opt/szip/2.1/intel/19/include")
 
 family("szip")
 
-prepend_path("CPATH",           "/util/opt/szip/2.1.1/intel/19/include")
-prepend_path("LIBRARY_PATH",    "/util/opt/szip/2.1.1/intel/19/lib")
-setenv("SZIP",                  "/util/opt/szip/2.1.1/intel/19")
+prepend_path("CPATH",           "/util/opt/szip/2.1/intel/19/include")
+prepend_path("LIBRARY_PATH",    "/util/opt/szip/2.1/intel/19/lib")
+setenv("SZIP",                  "/util/opt/szip/2.1/intel/19")
