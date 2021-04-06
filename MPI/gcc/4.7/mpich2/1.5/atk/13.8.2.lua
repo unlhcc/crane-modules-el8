7953e744fb84a68fc174303a6b05904caf8ee431
Adam Caprez <acaprez2@unl.edu>
2021-04-06 23:45:41 +0000

    Remove ancient OMPI.
    [skip ci]

---
+++ MPI/gcc/4.7/mpich2/1.5/atk/13.8.2.lua
@@ -0,0 +1,27 @@
+local help_message = [[
+ATK 13.8.2
+
+This module loads the ATK environment, prebuilt with MPICH2
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: ATK")
+whatis("Version: 13.8.2")
+whatis("Category: Application, Chemistry")
+whatis("Keywords: Application, Chemistry")
+whatis("URL: http://www.quantumwise.com/")
+
+setenv("QUANTUM_LICENSE_PATH", "6200@dmol4b.unl.edu")
+setenv("LMX_LICENSE_PATH", "6200@dmol4b.unl.edu")
+
+prepend_path("PATH","/util/opt/atk/13.8.2/vnl/bin")
+prepend_path("PATH","/util/opt/atk/13.8.2/atkpython/bin")
+prepend_path("PATH","/util/opt/atk/13.8.2/license")
+
+-- Workaround for RHEL 6.5 upgrade
+-- See http://quantumwise.com/forum/index.php?topic=2564.15#.U-zVMlb9O0s
+setenv("LD_PRELOAD",	"libstdc++.so.6")
+
+family("atk")
