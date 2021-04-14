41e6c3674729db4b12fbccec8718a52b58190b9e
Adam Caprez <acaprez2@unl.edu>
2021-04-14 23:58:50 +0000

    Add SLURM workaround vars to mpich.

--- Compiler/gcc/5.4/mpich/3.2.lua
+++ Compiler/gcc/5.4/mpich/3.2.lua
@@ -33,3 +33,6 @@ family("mpi")
 prepend_path("CPATH",             "/util/opt/mpich/3.2/gcc/5.4/include")
 prepend_path("LIBRARY_PATH",     "/util/opt/mpich/3.2/gcc/5.4/lib")
 prepend_path("PKG_CONFIG_PATH",        "/util/opt/mpich/3.2/gcc/5.4/lib/pkgconfig")
+
+setenv("SLURM_WHOLE","1")
+setenv("SLURM_OVERLAP","1")
