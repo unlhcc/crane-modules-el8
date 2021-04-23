da38ee4691fb894f727ef1f51dce5b11f35420ad
Adam Caprez <acaprez2@unl.edu>
2021-04-23 22:19:52 +0000

    More lapack modules.

---
+++ Compiler/intel/17/lapack/3.9.lua
@@ -0,0 +1,20 @@
+help(
+[[
+This module load the reference implemantation of BLAS and LAPACK from netlib.org.
+
+Version 3.9
+]]
+)
+
+whatis("Name: lapack")
+whatis("Version: 3.9.1")
+whatis("Category: Utility, Mathematics")
+whatis("Keywords: Utility, Mathematics")
+whatis("URL: http://www.netlib.org/lapack/")
+whatis("Description: LAPACK is written in Fortran 90 and provides routines for solving systems of simultaneous linear equations, least-squares solutions of linear systems of equations, eigenvalue problems, and singular value problems.")
+
+
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/lapack/3.9/intel/17/lib")
+prepend_path("LIBRARY_PATH",   "/util/opt/lapack/3.9/intel/17/lib")
+prepend_path("PKG_CONFIG_PATH","/util/opt/lapack/3.9/intel/17/lib/pkgconfig")
+setenv("LAPACK", "/util/opt/lapack/3.9/intel/17")
