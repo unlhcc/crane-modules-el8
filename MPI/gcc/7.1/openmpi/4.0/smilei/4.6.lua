92a6c91916b660f630c53dd8968318b639ed5839
Adam Caprez <acaprez2@unl.edu>
2021-04-23 09:36:51 +0000

    Add module file for smilei 4.6.

---
+++ MPI/gcc/7.1/openmpi/4.0/smilei/4.6.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+smilei 4.6
+
+This module loads the smilei environment, built with the GCC+OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: smilei")
+whatis("Version: 4.6")
+whatis("Category: Physics, Simulation")
+whatis("Keywords: Physics, Simulation")
+whatis("Description: Smilei is a Particle-In-Cell code for plasma simulation.")
+whatis("URL: https://smileipic.github.io")
+
+load("phdf5/1.12")
+prereq("python/3.8")
+prepend_path("PATH",                "/util/opt/smilei/4.6/openmpi/4.0/gcc/7.1/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/smilei/4.6/openmpi/4.0/gcc/7.1/lib")
+prepend_path("LIBRARY_PATH",        "/util/opt/smilei/4.6/openmpi/4.0/gcc/7.1/lib")
+setenv("PICSAR",                     "/util/opt/smilei/4.6/openmpi/4.0/gcc/7.1")
+
+family("smilei")
