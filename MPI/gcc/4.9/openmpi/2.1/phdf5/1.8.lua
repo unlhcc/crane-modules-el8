a642faaf987bc853cda3d6cdd7eb46a2f42ef8a3
Adam Caprez <acaprez2@unl.edu>
2021-04-30 01:17:01 +0000

    phdf5 w/gcc 4.8 and ompi 2.1

---
+++ MPI/gcc/4.9/openmpi/2.1/phdf5/1.8.lua
@@ -0,0 +1,26 @@
+local help_message = [[
+Parallel hdf5 1.8.15
+
+This module loads the hdf5, built with the GCC,OpenMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.8.15")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/lib")
+prepend_path("MANPATH",             "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/share")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/include")
+prepend_path("CPATH",               "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9/lib")
+
+load("szip/2.1","zlib/1.2")
+family("hdf5")
+setenv("PHDF5",        "/util/opt/hdf5/1.8/openmpi/2.1/gcc/4.9")
