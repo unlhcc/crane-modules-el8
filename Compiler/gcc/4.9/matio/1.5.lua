5b260fa1b68029cf62407fda640e4cda51347186
Adam Caprez <acaprez2@unl.edu>
2021-04-30 22:06:27 +0000

    Update to latest.

--- Compiler/gcc/4.9/matio/1.5.lua
+++ Compiler/gcc/4.9/matio/1.5.lua
@@ -9,19 +9,19 @@ compilers.
 help(help_message,"\n")
 
 whatis("Name: matio")
-whatis("Version: 1.5.6")
+whatis("Version: 1.5.21")
 whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: https://sourceforge.net/projects/matio/")
 
-prepend_path("PATH",                "/util/opt/matio/1.5.6/gcc/4.9/bin")
-prepend_path("LD_LIBRARY_PATH",     "/util/opt/matio/1.5.6/gcc/4.9/lib")
-prepend_path("MANPATH",             "/util/opt/matio/1.5.6/gcc/4.9/share/man")
-prepend_path("INCLUDE",             "/util/opt/matio/1.5.6/gcc/4.9/include")
+prepend_path("PATH",                "/util/opt/matio/1.5/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/matio/1.5/gcc/4.9/lib")
+prepend_path("MANPATH",             "/util/opt/matio/1.5/gcc/4.9/share/man")
+prepend_path("INCLUDE",             "/util/opt/matio/1.5/gcc/4.9/include")
 
 family("matio")
 
-prepend_path("CPATH",               "/util/opt/matio/1.5.6/gcc/4.9/include")
-prepend_path("LIBRARY_PATH",        "/util/opt/matio/1.5.6/gcc/4.9/lib")
-prepend_path("PKG_CONFIG_PATH",     "/util/opt/matio/1.5.6/gcc/4.9/lib/pkgconfig")
-setenv("MATIO",              "/util/opt/matio/1.5.6/gcc/4.9")
+prepend_path("CPATH",               "/util/opt/matio/1.5/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/matio/1.5/gcc/4.9/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/matio/1.5/gcc/4.9/lib/pkgconfig")
+setenv("MATIO",              "/util/opt/matio/1.5/gcc/4.9")
