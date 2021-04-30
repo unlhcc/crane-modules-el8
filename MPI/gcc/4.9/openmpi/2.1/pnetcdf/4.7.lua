9abe8a85cc6781b785b436f8b562422a4181c510
Adam Caprez <acaprez2@unl.edu>
2021-04-30 01:29:21 +0000

    Add pnetcdf 4.7, built w/GCC 4.9 and OMPI 2.1.

---
+++ MPI/gcc/4.9/openmpi/2.1/pnetcdf/4.7.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+netCDF 4.7
+
+This module loads the netCDF, built with the GCC 
+compilers and OpenMPI.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: netCDF")
+whatis("Version: 4.7")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.unidata.ucar.edu/downloads/netcdf/index.jsp")
+
+setenv("PNETCDF",              "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9")
+prepend_path("PATH",                "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/lib")
+prepend_path("INCLUDE",             "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/include")
+prepend_path("CPATH",               "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/netcdf/4.7/openmpi/2.1/gcc/4.9/lib/pkgconfig")
+
+family("netcdf")
