eb96799540a226160cbe90f01489c51c7004374a
Adam Caprez <acaprez2@unl.edu>
2021-05-01 03:00:54 +0000

    Add trilinos built w/GCC 4.9 and OMPI 2.1.

---
+++ MPI/gcc/4.9/openmpi/2.1/trilinos/12.6.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+Trilinos 12.6
+
+The Trilinos Project is an effort to develop algorithms and enabling technologies within an object-oriented software framework for the solution of large-scale, complex multi-physics engineering and scientific problems. A unique design feature of Trilinos is its focus on packages.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: Trilinos")
+whatis("Version: 12.6")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://trilinos.org/")
+
+setenv("TRILINOS", "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9")
+prepend_path("PATH",                "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9/lib")
+prepend_path("INCLUDE",             "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9/include")
+prepend_path("CPATH",               "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/trilinos/12.6/openmpi/2.1/gcc/4.9/lib")
+
+family("trilinos")
+
