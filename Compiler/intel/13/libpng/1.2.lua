d186c0cf605cf7d57ff646627bb4df39f3cf13db
Adam Caprez <acaprez2@unl.edu>
2021-05-05 22:06:16 +0000

    Restore libpng 1.2 modules.

---
+++ Compiler/intel/13/libpng/1.2.lua
@@ -0,0 +1,27 @@
+local help_message = [[
+libpng 1.2.50
+
+This module loads the libpng, built with the Intel
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: libpng")
+whatis("Version: 1.2.50")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.libpng.org/")
+
+prepend_path("PATH",                "/util/opt/libpng/1.2/intel/13/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/libpng/1.2/intel/13/lib")
+prepend_path("MANPATH",             "/util/opt/libpng/1.2/intel/13/doc")
+prepend_path("INCLUDE",             "/util/opt/libpng/1.2/intel/13/include")
+
+family("libpng")
+
+prepend_path("CPATH",             "/util/opt/libpng/1.2/intel/13/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/libpng/1.2/intel/13/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/libpng/1.2/intel/13/lib/pkgconfig")
+setenv("LIBPNG", "/util/opt/libpng/1.2/intel/13")
