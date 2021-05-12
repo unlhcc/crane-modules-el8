0b4497e8e586df60be6c21165e33c0002ed8f7c8
Adam Caprez <acaprez2@unl.edu>
2021-05-12 04:04:21 +0000

    Update default nano to 5.7.

--- Core/crane.lua
+++ Core/crane.lua
@@ -6,4 +6,4 @@ Crane.
 help(help_message,"\n")
 
 whatis("Name: Crane Default Modules")
-load("compiler/gcc/10.2","openmpi/4.0","python/3.8","perl/5.26","git/2.30","nano/5.5")
+load("compiler/gcc/10.2","openmpi/4.0","python/3.8","perl/5.26","git/2.30","nano/5.7")
