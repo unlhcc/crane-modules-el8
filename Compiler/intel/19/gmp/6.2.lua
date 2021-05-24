b262bc063da54e2ddf795dff78db4f0c3978e24a
Adam Caprez <acaprez2@unl.edu>
2021-05-24 21:36:55 +0000

    Fix Intel GMP 6.2 modules.

--- Compiler/intel/19/gmp/6.2.lua
+++ Compiler/intel/19/gmp/6.2.lua
@@ -23,3 +23,5 @@ family("gmp")
 prepend_path("CPATH",             "/util/opt/gmp/6.2/intel/19/include")
 prepend_path("LIBRARY_PATH",     "/util/opt/gmp/6.2/intel/19/lib")
 prepend_path("PKG_CONFIG_PATH",        "/util/opt/gmp/6.2/intel/19/lib/pkgconfig")
+
+setenv("GMP", "/util/opt/gmp/6.2/intel/19")
