44610e626fa58a3b6287159f7522b251f75c9389
Adam Caprez <acaprez2@unl.edu>
2021-05-26 02:45:13 +0000

    Abaqus fixes.

--- Core/abaqus/2021.lua
+++ Core/abaqus/2021.lua
@@ -20,3 +20,14 @@ prepend_path("PATH","/util/opt/abaqus/2021/Commands")
 unsetenv("SLURM_GTIDS")
 setenv("SLURM_WHOLE", "1")
 setenv("SLURM_OVERLAP", "1")
+-- workaround for Intel 16.1 PID bug
+-- https://community.intel.com/t5/Intel-Fortran-Compiler/segfault-reading-from-piped-stdin-on-centos7-2/m-p/1065768#M118422
+setenv("FORT0", "/dev/stderr")
+setenv("FORT5", "/dev/stdin")
+setenv("FORT6", "/dev/stdout")
+
+-- external libfabric so PSM/PSM2 is used
+prepend_path("PATH","/util/opt/libfabric/1.11/gcc/8.3/bin")
+prepend_path("LD_LIBRARY_PATH","/util/opt/libfabric/1.11/gcc/8.3/lib")
+setenv("I_MPI_FABRICS",         "shm:ofi")
+setenv("I_MPI_OFI_LIBRARY_INTERNAL", "0")
