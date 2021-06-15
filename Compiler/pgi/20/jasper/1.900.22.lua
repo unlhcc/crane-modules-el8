38a14b5664282c09050b0123beedca21a349bd0f
Adam Caprez <acaprez2@unl.edu>
2021-06-15 21:31:04 +0000

    Add module file for 1.900.22.
    Newer jasper versions don't work with WPS 3.9.x.
    See https://forum.wrfforum.com/viewtopic.php?f=5&t=10248.

---
+++ Compiler/pgi/20/jasper/1.900.22.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+jasper 1.900.22
+
+This module loads the jasper, built with the PGI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: jasper")
+whatis("Version: 1.900.22")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://www.ece.uvic.ca/~frodo/jasper/")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/jasper/1.900.22/pgi/20/lib")
+prepend_path("MANPATH",             "/util/opt/jasper/1.900.22/pgi/20/share/man")
+prepend_path("INCLUDE",             "/util/opt/jasper/1.900.22/pgi/20/include")
+prepend_path("CPATH",               "/util/opt/jasper/1.900.22/pgi/20/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/jasper/1.900.22/pgi/20/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/jasper/1.900.22/pgi/20/lib/pkgconfig")
+setenv("JASPER","/util/opt/jasper/1.900.22/pgi/20")
+setenv("JASPERINC","/util/opt/jasper/1.900.22/pgi/20/include")
+setenv("JASPERLIB","/util/opt/jasper/1.900.22/pgi/20/lib")
