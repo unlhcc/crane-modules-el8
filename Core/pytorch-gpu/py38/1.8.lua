07e3d8484de457d0e16edb21656224cb28792cda
Adam Caprez <acaprez2@unl.edu>
2021-06-21 17:37:07 +0000

    Add python family.

--- Core/pytorch-gpu/py38/1.8.lua
+++ Core/pytorch-gpu/py38/1.8.lua
@@ -17,3 +17,5 @@ whatis("Description: Tensors and Dynamic neural networks in Python with strong G
 pushenv("CONDA_DEFAULT_ENV", "pytorch-gpu-1.8.0-py38")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch-gpu/envs")
 prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch-gpu/envs/pytorch-gpu-1.8.0-py38/bin")
+
+family("python")
