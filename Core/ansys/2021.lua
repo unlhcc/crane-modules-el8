6ad947bba9b6f9094ca6f32be93721f95dd2314e
Adam Caprez <acaprez2@unl.edu>
2021-08-09 18:08:58 -0500

    Fix typo.

--- Core/ansys/2021.lua
+++ Core/ansys/2021.lua
@@ -12,8 +12,7 @@ whatis("Name: ansys")
 whatis("Version: 2021R2")
 whatis("Category: Application, Engineering, FEA")
 whatis("Keywords: Application, Engineering, FEA")
-whatis("Description: ANSYS offers a comprehensive software suite that spans the entire range of physics, providing access to virtually any field of engineering s
-imulation that a design process requires.")
+whatis("Description: ANSYS offers a comprehensive software suite that spans the entire range of physics, providing access to virtually any field of engineering simulation that a design process requires.")
 whatis("URL: https://www.ansys.com")
 
 prepend_path("PATH",  "/util/opt/ansys/2021R2/v212/Framework/bin/Linux64")
