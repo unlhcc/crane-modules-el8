09d6c772eb55c004db641bbea2da0a51c5a36dc9
Emelie Harstad <eharstad@unl.edu>
2021-08-10 14:51:42 +0000

    Updating rosettafold module

--- Core/rosettafold/1.0.lua
+++ Core/rosettafold/1.0.lua
@@ -4,6 +4,19 @@ This module loads RoseTTAFold.
 Version 1.0.0
 ]]
 )
+if mode() == "load" then
+  LmodMessage("-------------------------| Usage of RoseTTAFold |--------------------------------")
+  LmodMessage("After loading the module, create a copy of the network and weights directories")
+  LmodMessage("in your home directory.")
+  LmodMessage("This only needs to be done once the first time the module is loaded.")
+  LmodMessage("")
+  LmodMessage("cp -r ${ROSETTAFOLD_NETWORK:-none} ~/")
+  LmodMessage("cp -r ${ROSETTAFOLD_WEIGHTS:-none} ~/")
+  LmodMessage("")
+  LmodMessage("Usage: run_[pyrosetta, e2e]_ver.sh <input_fasta_file> <output_dir>")
+  LmodMessage("---------------------------------------------------------------------------------")
+end
+
 whatis("Name: RoseTTAFold")
 whatis("Version: 1.0.0")
 whatis("Category: bioinformatics, protein design")
@@ -14,5 +27,6 @@ whatis("Description: RoseTTAFold - Accurate prediction of protein structures and
 load("anaconda")
 append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/rosettafold/envs")
 prepend_path("PATH", "/util/opt/rosettafold/1.0.0")
-
+setenv("ROSETTAFOLD_NETWORK","/util/opt/rosettafold/1.0.0/network")
+setenv("ROSETTAFOLD_WEIGHTS","/util/opt/rosettafold/1.0.0/weights")
 
