43af6be898d96aa4c31bc8a584a33ab0cd72210c
Adam Caprez <acaprez2@unl.edu>
2021-08-12 16:53:27 -0500

    Update cplex modules to include python envs.

--- Core/ibm-ilog-cplex/12.10.lua
+++ Core/ibm-ilog-cplex/12.10.lua
@@ -14,7 +14,15 @@ whatis("Keywords: CPLEX, IBM Optimization Studio")
 whatis("URL: https://www.ibm.com/products/ilog-cplex-optimization-studio")
 
 prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/cplex/bin/x86-64_linux")
+prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/cpoptimizer/bin/x86-64_linux")
+prepend_path("PATH",     "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/opl/oplide")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/cplex/lib/x86-64_linux/static_pic")
 prepend_path("INCLUDE",             "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/cplex/include/ilcplex/")
 prepend_path("ILOG_CPLEX_PATH",     "/util/opt/IBM-ILOG-CPLEX/12.10/intel/19.0.1/")
 
+-- python env containing cplex package and dependencies
+pushenv("CONDA_DEFAULT_ENV", "ilog-cplex-12.10")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ilog-cplex/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/ilog-cplex/envs/ilog-cplex-12.10/bin")
+
+family("python")
