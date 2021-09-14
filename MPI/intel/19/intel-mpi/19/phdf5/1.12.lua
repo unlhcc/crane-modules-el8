84da9420b482f885ff2c9e702a904dc67c72297c
Adam Caprez <acaprez2@unl.edu>
2021-09-14 17:26:14 -0500

    Phdf5

---
+++ MPI/intel/19/intel-mpi/19/phdf5/1.12.lua
@@ -0,0 +1,26 @@
+local help_message = [[
+Parallel hdf5 1.12.0
+
+This module loads hdf5, built with the Intel+IMPI
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: hdf5")
+whatis("Version: 1.12.0")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.hdfgroup.org/HDF5/release/obtain5.html#obtain")
+
+prepend_path("PATH",                "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/lib")
+prepend_path("MANPATH",             "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/share")
+prepend_path("INCLUDE",             "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/lib")
+prepend_path("CPATH",               "/util/opt/hdf5/1.12/intel-mpi/19/intel/19/include")
+setenv("PHDF5","/util/opt/hdf5/1.12/intel-mpi/19/intel/19")
+
+family("hdf5")
+load("szip/2.1","zlib/1.2")
