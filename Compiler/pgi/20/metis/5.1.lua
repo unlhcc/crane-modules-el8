5aabb1081dde5faaa0c7024dae6c20dbc2ab5594
Adam Caprez <acaprez2@unl.edu>
2021-09-16 20:04:11 -0500

    Add additional metis modules.

--- Compiler/pgi/20/metis/5.1.lua
+++ Compiler/pgi/20/metis/5.1.lua
@@ -1,7 +1,7 @@
 local help_message = [[
 METIS 5.1
 
-This module loads the Metis environment, built with the GCC 10.2
+This module loads the Metis environment, built with the PGI 20
 compilers.
 
 ]]
@@ -14,8 +14,8 @@ whatis("Category: library, runtime support")
 whatis("Keywords: Application, partitioning graphs")
 whatis("URL: http://glaros.dtc.umn.edu/gkhome/metis/metis/overview")
 
-prepend_path("PATH","/util/opt/metis/5.1/gcc/10.2/bin")
-prepend_path("LD_LIBRARY_PATH",     "/util/opt/metis/5.1/gcc/10.2/lib/")
-setenv("METIS_INCLUDE",             "/util/opt/metis/5.1/gcc/10.2/include/")
-setenv("METIS","/util/opt/metis/5.1/gcc/10.2")
+prepend_path("PATH","/util/opt/metis/5.1/pgi/20/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/metis/5.1/pgi/20/lib/")
+setenv("METIS_INCLUDE",             "/util/opt/metis/5.1/pgi/20/include/")
+setenv("METIS","/util/opt/metis/5.1/pgi/20")
 setenv("METIS_VERSION","5.1.0")
