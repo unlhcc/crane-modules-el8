3671d44be9336ca3b3e82a580f8da1ec15fd0fb3
Adam Caprez <acaprez2@unl.edu>
2021-09-28 17:27:39 -0500

    PETSc w/GCC 9.1 and OMPI 3.1.

---
+++ MPI/gcc/9.1/openmpi/3.1/petsc/3.2.lua
@@ -0,0 +1,25 @@
+local help_message = [[
+PETSc 3.2
+
+PETSc, pronounced PET-see (the S is silent), is a suite of data structures and routines for the scalable (parallel) solution of scientific applications modeled by partial differential equations. It supports MPI, and GPUs through CUDA or OpenCL, as well as hybrid MPI-GPU parallelism.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: PETSc")
+whatis("Version: 3.2")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://www.mcs.anl.gov/petsc/index.html")
+
+setenv("PETSC_DIR",                 "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1")
+
+prepend_path("PATH",                "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/bin")
+prepend_path("PATH",                "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/lib/petsc/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/lib")
+prepend_path("MANPATH",             "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/share")
+prepend_path("INCLUDE",             "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/include")
+prepend_path("CPATH",               "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/petsc/3.2/openmpi/3.1/gcc/9.1/lib")
+
+load("intel-mkl/19")
