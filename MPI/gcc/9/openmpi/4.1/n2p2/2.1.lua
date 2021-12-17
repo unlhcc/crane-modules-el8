474e4d899c5cdc557833bad8b37cb465700a91a0
Adam Caprez <acaprez2@unl.edu>
2021-12-17 16:05:58 -0600

    Add module file for n2p2 2.1.

---
+++ MPI/gcc/9/openmpi/4.1/n2p2/2.1.lua
@@ -0,0 +1,17 @@
+help (
+[[
+This module loads n2p2 built w/GCC 9.
+
+Version 2.1.4
+]])
+
+whatis("Name: n2p2")
+whatis("Version: 2.1.4")
+whatis("Category: Chemistry, Physics")
+whatis("Keywords: Chemistry, Physics")
+whatis("Description: n2p2 - A Neural Network Potential Package.")
+whatis("URL: https://compphysvienna.github.io/n2p2/")
+
+
+pushenv("CONDA_DEFAULT_ENV", "n2p2-2.1.4")
+prepend_path("PATH"    ,"/util/opt/anaconda/deployed-conda-envs/packages/n2p2/envs/n2p2-2.1.4/bin")
