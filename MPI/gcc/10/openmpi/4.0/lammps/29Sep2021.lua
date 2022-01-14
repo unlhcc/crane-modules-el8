a9e2ad12b3cc441f41719bfe6a0a86b3d78cab97
Adam Caprez <acaprez2@unl.edu>
2022-01-13 21:35:33 -0600

    Update lammps module.

--- MPI/gcc/10/openmpi/4.0/lammps/29Sep2021.lua
+++ MPI/gcc/10/openmpi/4.0/lammps/29Sep2021.lua
@@ -14,8 +14,10 @@ whatis("Category: library, runtime support")
 whatis("Keywords: System, Library")
 whatis("URL: http://lammps.sandia.gov/")
 
-setenv("LAMMPS_POTENTIALS",         "/util/opt/lammps/potentials")
+setenv("LAMMPS_POTENTIALS",         "/util/opt/lammps/29Sep2021_update2/openmpi/4.0/gcc/10/share/lammps/potentials")
+setenv("MSI2LMP_LIBRARY",           "/util/opt/lammps/29Sep2021_update2/openmpi/4.0.5/gcc/10/share/lammps/frc_files")
 prepend_path("PATH",                "/util/opt/lammps/29Sep2021_update2/openmpi/4.0/gcc/10/bin")
 prepend_path("LD_LIBRARY_PATH",     "/util/opt/lammps/29Sep2021_update2/openmpi/4.0/gcc/10/lib64")
 
 family("lammps")
+load("fftw3/3.3")
