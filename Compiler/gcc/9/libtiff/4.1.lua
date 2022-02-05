edb37a8093a54fdb5307288d3f0ccd4eb4167c49
Adam Caprez <acaprez2@unl.edu>
2022-02-04 18:36:31 -0600

    Add additional libtiff builds.

---
+++ Compiler/gcc/9/libtiff/4.1.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+libtiff 4.1
+
+This module loads the libtiff, built with the GCC compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: libtiff")
+whatis("Version: 4.1.0")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.libtiff.org/")
+
+prepend_path("PATH",                "/util/opt/libtiff/4.1/gcc/9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/libtiff/4.1/gcc/9/lib")
+prepend_path("MANPATH",             "/util/opt/libtiff/4.1/gcc/9/doc")
+prepend_path("INCLUDE",             "/util/opt/libtiff/4.1/gcc/9/include")
+
+prepend_path("CPATH",             "/util/opt/libtiff/4.1/gcc/9/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/libtiff/4.1/gcc/9/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/libtiff/4.1/gcc/9/lib/pkgconfig")
+setenv("LIBTIFF", "/util/opt/libtiff/4.1/gcc/9")
