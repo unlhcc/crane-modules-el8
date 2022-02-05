f24f37d82480e18fa50a5962c1215eb96f1f9d70
Adam Caprez <acaprez2@unl.edu>
2022-02-04 19:52:14 -0600

    Add module files for lcms2/2.13.

---
+++ Compiler/pgi/20/lcms2/2.13.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+lcms2 2.13
+
+This module loads the lcms2, built with the PGI compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: lcms2")
+whatis("Version: 2.13.1")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://www.littlecms.com")
+
+prepend_path("PATH",                "/util/opt/lcms2/2.13/pgi/20/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/lcms2/2.13/pgi/20/lib")
+prepend_path("MANPATH",             "/util/opt/lcms2/2.13/pgi/20/doc")
+prepend_path("INCLUDE",             "/util/opt/lcms2/2.13/pgi/20/include")
+
+prepend_path("CPATH",             "/util/opt/lcms2/2.13/pgi/20/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/lcms2/2.13/pgi/20/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/lcms2/2.13/pgi/20/lib/pkgconfig")
+setenv("LCMS2", "/util/opt/lcms2/2.13/pgi/20")
