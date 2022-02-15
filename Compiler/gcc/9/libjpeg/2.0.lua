3ebbcb9f2faf908de876d4f1cbdaa65f3f415359
Adam Caprez <acaprez2@unl.edu>
2022-02-14 19:03:18 -0600

    Add addt'l modules for libjpeg(-turbo).

---
+++ Compiler/gcc/9/libjpeg/2.0.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+libjpeg-turbo 2.0
+
+This module loads libjpeg-turbo, built with the GCC compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: libjpeg-turbo")
+whatis("Version: 2.0.5")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.libjpeg-turbo.org/")
+
+prepend_path("PATH",                "/util/opt/libjpeg-turbo/2.0/gcc/9/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/libjpeg-turbo/2.0/gcc/9/lib")
+prepend_path("MANPATH",             "/util/opt/libjpeg-turbo/2.0/gcc/9/doc")
+prepend_path("INCLUDE",             "/util/opt/libjpeg-turbo/2.0/gcc/9/include")
+
+prepend_path("CPATH",             "/util/opt/libjpeg-turbo/2.0/gcc/9/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/libjpeg-turbo/2.0/gcc/9/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/libjpeg-turbo/2.0/gcc/9/lib/pkgconfig")
+setenv("LIBJPEG", "/util/opt/libjpeg-turbo/2.0/gcc/9")
---
+++ Compiler/intel/19/libjpeg/2.0.lua
@@ -0,0 +1,24 @@
+local help_message = [[
+libjpeg-turbo 2.0
+
+This module loads libjpeg-turbo, built with the Intel compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: libjpeg-turbo")
+whatis("Version: 2.0.5")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://www.libjpeg-turbo.org/")
+
+prepend_path("PATH",                "/util/opt/libjpeg-turbo/2.0/intel/19/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/libjpeg-turbo/2.0/intel/19/lib")
+prepend_path("MANPATH",             "/util/opt/libjpeg-turbo/2.0/intel/19/doc")
+prepend_path("INCLUDE",             "/util/opt/libjpeg-turbo/2.0/intel/19/include")
+
+prepend_path("CPATH",             "/util/opt/libjpeg-turbo/2.0/intel/19/include")
+prepend_path("LIBRARY_PATH",     "/util/opt/libjpeg-turbo/2.0/intel/19/lib")
+prepend_path("PKG_CONFIG_PATH",        "/util/opt/libjpeg-turbo/2.0/intel/19/lib/pkgconfig")
+setenv("LIBJPEG", "/util/opt/libjpeg-turbo/2.0/intel/19")
