d7caf7b7ab59fa139782bcd69a4ced479bf70483
Adam Caprez <acaprez2@unl.edu>
2022-02-18 19:40:19 -0600

    Add module files for minpack 19961126.

---
+++ Compiler/pgi/20/minpack/19961126.lua
@@ -0,0 +1,19 @@
+help(
+[[
+This module load the reference implemantation of MINPACK from netlib.org.
+
+Version 19961126
+]]
+)
+
+whatis("Name: minpack")
+whatis("Version: 19961126")
+whatis("Category: Utility, Mathematics")
+whatis("Keywords: Utility, Mathematics")
+whatis("URL: http://www.netlib.org/minpack/")
+whatis("Description: Minpack includes software for solving nonlinear equations and nonlinear least squares problems.")
+
+prepend_path("LD_LIBRARY_PATH",   "/util/opt/minpack/19961126/pgi/20/lib")
+prepend_path("LIBRARY_PATH",   "/util/opt/minpack/19961126/pgi/20/lib")
+prepend_path("CPATH",  "/util/opt/minpack/19961126/pgi/20/lib")
+setenv("MINPACK", "/util/opt/minpack/19961126/pgi/20")
