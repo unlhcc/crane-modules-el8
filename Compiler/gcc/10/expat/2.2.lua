cd3c1b90f632a7199c70a2a4a7f408319ed01f77
Adam Caprez <acaprez2@unl.edu>
2022-02-22 23:48:19 -0600

    Add addt'l expat modules.

---
+++ Compiler/gcc/10/expat/2.2.lua
@@ -0,0 +1,23 @@
+local help_message = [[
+expat 2.2
+
+This module loads the expat, built with the GCC
+compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: expat")
+whatis("Version: 2.2.9")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: https://github.com/libexpat/libexpat")
+
+prepend_path("PATH",                "/util/opt/expat/2.2/gcc/10/bin")
+prepend_path("CPATH",               "/util/opt/expat/2.2/gcc/10/include")
+prepend_path("LIBRARY_PATH",        "/util/opt/expat/2.2/gcc/10/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/expat/2.2/gcc/10/lib")
+prepend_path("PKG_CONFIG_PATH",     "/util/opt/expat/2.2/gcc/10/lib/pkgconfig")
+setenv("EXPAT", "/util/opt/expat/2.2/gcc/10")
+family("expat")
