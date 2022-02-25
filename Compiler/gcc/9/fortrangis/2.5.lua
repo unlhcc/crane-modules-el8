986bd26507f0f97eb55bb2def0c48a96f85109c5
Adam Caprez <acaprez2@unl.edu>
2022-02-25 15:46:39 -0600

    Add module files for FortranGIS 2.5.

---
+++ Compiler/gcc/9/fortrangis/2.5.lua
@@ -0,0 +1,20 @@
+local help_message = [[
+FortranGIS 2.5
+
+This module loads FortranGIS, built with the GCC compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: fortrangis")
+whatis("Version: 2.5")
+whatis("Category: library, runtime support")
+whatis("Keywords: System, Library")
+whatis("URL: http://fortrangis.sourceforge.net/index.php")
+
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/fortrangis/2.5/gcc/9/lib")
+prepend_path("LIBRARY_PATH",     "/util/opt/fortrangis/2.5/gcc/9/lib")
+prepend_path("CPATH",           "/util/opt/fortrangis/2.5/gcc/9/include")
+
+setenv("FORTRANGIS",                  "/util/opt/fortrangis/2.5/gcc/9")
