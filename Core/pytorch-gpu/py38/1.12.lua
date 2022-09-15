8c00bd0d0a7e2741e2f6264fc76fd69c9b5e48c1
Adam Caprez <acaprez2@unl.edu>
2022-09-14 23:02:18 -0500

    Add module files for pytorch-gpu 1.12.

---
+++ Core/pytorch-gpu/py38/1.12.lua
@@ -0,0 +1,21 @@
+help(
+[[
+This module loads pytorch-gpu.
+Version 1.12.1
+
+Note: This module also includes tensorflow-gpu 2.9.
+
+]]
+)
+whatis("Name: Pytorch-gpu")
+whatis("Version: 1.12.1")
+whatis("Category: deep learning, machine learning, mathematics")
+whatis("Keywords: deep learning, machine learning, mathematics")
+whatis("URL: https://pytorch.org/")
+whatis("Description: Tensors and Dynamic neural networks in Python with strong GPU acceleration")
+
+pushenv("CONDA_DEFAULT_ENV", "pytorch-gpu-1.12.1-py38")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch-gpu/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/pytorch-gpu/envs/pytorch-gpu-1.12.1-py38/bin")
+
+family("python")
