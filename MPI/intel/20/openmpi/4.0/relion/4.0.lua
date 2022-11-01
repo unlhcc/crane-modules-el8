226317472fc2e67e8606f210d9119c4957b084e0
Adam Caprez <acaprez2@unl.edu>
2022-11-01 10:29:37 -0500

    Add module file for relion 4.0.

---
+++ MPI/intel/20/openmpi/4.0/relion/4.0.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+RELION (for REgularised LIkelihood OptimisatioN) is a stand-alone computer program for Maximum A Posteriori refinement of (multiple) 3D reconstructions or 2D class averages in cryo-electron microscopy. It is developed in the research group of Sjors Scheres at the MRC Laboratory of Molecular Biology.
+
+Please note: This module only provides the RELION CLI tools. For GUI access, use the RELION Open OnDemand app.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: RELION ")
+whatis("Version: 4.0.0")
+whatis("Category: Physics, Chemistry")
+whatis("Keywords: Physics, Chemistry")
+whatis("URL: https://github.com/3dem/relion")
+
+prepend_path("PATH",                "/util/opt/relion/4.0/openmpi/4.0/intel/20/bin")
+prepend_path("LIBRARY_PATH",        "/util/opt/relion/4.0/openmpi/4.0/intel/20/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/opt/relion/4.0/openmpi/4.0/intel/20/lib")
+load("intel-mkl/20","zlib/1.2","libtiff/4.1","libpng/1.6","libjpeg/2.0","intel-tbb/2021")
