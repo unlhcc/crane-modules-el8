07157b5c0b93d40b78d7fafc85a28bdbf7c8a50d
Adam Caprez <acaprez2@unl.edu>
2022-12-08 16:04:15 -0600

    Add module file for GCC 12.

---
+++ Core/compiler/gcc/12.lua
@@ -0,0 +1,46 @@
+local help_message = [[
+GCC Compilers 12
+
+This module loads GCC Compiler variables.
+The command directory is added to PATH.
+The library directory is added to LD_LIBRARY_PATH.
+The include directory is added to INCLUDE.
+The man     directory is added to MANPATH.
+]]
+
+help(help_message,"\n")
+
+whatis("Name: GCC Compilers")
+whatis("Version: 12")
+whatis("Category: compiler")
+whatis("Keywords: System, compiler")
+whatis("URL: http://gcc.gnu.org")
+
+prepend_path("PATH",                "/util/comp/gcc/12/bin")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/gcc/12/lib")
+prepend_path("LD_LIBRARY_PATH",     "/util/comp/gcc/12/lib64")
+prepend_path("LIBRARY_PATH",     "/util/comp/gcc/12/lib")
+prepend_path("LIBRARY_PATH",     "/util/comp/gcc/12/lib64")
+prepend_path("MANPATH",             "/util/comp/gcc/12/share/man")
+prepend_path("INCLUDE",             "/util/comp/gcc/12/include")
+prepend_path("PKG_CONFIG_PATH",     "/util/comp/gcc/12/lib/pkgconfig")
+prepend_path("PKG_CONFIG_PATH",     "/util/comp/gcc/12/lib64/pkgconfig")
+setenv(      "GCC_LIB",             "/util/comp/gcc/12/lib64")
+
+local mroot = os.getenv("MODULEPATH_ROOT") or "/util/opt/modulefiles"
+local mdir = pathJoin(mroot,"Compiler","gcc","12")
+prepend_path("MODULEPATH",          mdir)
+
+setenv("CC",	"gcc")
+setenv("FC",	"gfortran")
+setenv("F90",	"gfortran")
+setenv("F77",	"gfortran")
+setenv("CXX",	"g++")
+
+append_path("CFLAGS","-march=sandybridge", " ")
+append_path("FFLAGS","-march=sandybridge", " ")
+append_path("CXXFLAGS","-march=sandybridge", " ")
+append_path("F90FLAGS","-march=sandybridge", " ")
+append_path("FCFLAGS","-march=sandybridge", " ")
+
+family("compiler")
