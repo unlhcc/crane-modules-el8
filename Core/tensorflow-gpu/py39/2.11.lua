59b17204f009c19434033b35a9dd4e49db73f755
Adam Caprez <acaprez2@unl.edu>
2023-02-14 23:04:09 -0600

    Add module files for tensorflow-gpu 2.10 and 2.11.

---
+++ Core/tensorflow-gpu/py39/2.11.lua
@@ -0,0 +1,18 @@
+help(
+[[
+This module loads Tensorflow-gpu.
+Version 2.11.0
+]]
+)
+whatis("Name: Tensorflow-gpu")
+whatis("Version: 2.11.0")
+whatis("Category: machine learning, mathematics")
+whatis("Keywords: Machine learning, Mathematics")
+whatis("URL: http://tensorflow.org/")
+whatis("Description: TensorFlow is an open source machine learning framework for everyone.")
+
+pushenv("CONDA_DEFAULT_ENV", "tensorflow-gpu-2.11.0-py39")
+append_path("CONDA_ENVS_PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs")
+prepend_path("PATH", "/util/opt/anaconda/deployed-conda-envs/packages/tensorflow-gpu/envs/tensorflow-gpu-2.11.0-py39/bin")
+
+family("python")
