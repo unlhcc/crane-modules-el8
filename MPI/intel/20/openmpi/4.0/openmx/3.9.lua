b4c20df04b5963c1db1403f424291d558b89a3fd
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-02-28 13:30:11 -0600

    Add module file for OpenMX

---
+++ MPI/intel/20/openmpi/4.0/openmx/3.9.lua
@@ -0,0 +1,19 @@
+local help_message = [[
+OpenMX 3.9
+
+This module loads the OpenMX environment, built with the intel 20 compilers.
+
+]]
+
+help(help_message,"\n")
+
+whatis("Name: OpenMX")
+whatis("Version: 3.9")
+whatis("Category: library, runtime support")
+whatis("Keywords: Applications, Chemistry")
+whatis("URL: http://www.openmx-square.org/download.html")
+
+load("intel-mkl/20","ftw3/3.3")
+
+prepend_path("PATH","/util/opt/openmx/3.9/openmpi/4.0/intel/20/source")
+prepend_path("PATH","/util/opt/openmx/3.9/openmpi/4.0/intel/20/work")
