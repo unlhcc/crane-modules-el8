efb8fc383b2e7ad51e85c8b0dfb8112e01147bfa
Natasha Pavlovikj <npavlovikj2@unl.edu>
2023-02-28 13:58:45 -0600

    Fix typo

--- MPI/intel/20/openmpi/4.0/openmx/3.9.lua
+++ MPI/intel/20/openmpi/4.0/openmx/3.9.lua
@@ -13,7 +13,7 @@ whatis("Category: library, runtime support")
 whatis("Keywords: Applications, Chemistry")
 whatis("URL: http://www.openmx-square.org/download.html")
 
-load("intel-mkl/20","ftw3/3.3")
+load("intel-mkl/20","fftw3/3.3")
 
 prepend_path("PATH","/util/opt/openmx/3.9/openmpi/4.0/intel/20/source")
 prepend_path("PATH","/util/opt/openmx/3.9/openmpi/4.0/intel/20/work")
